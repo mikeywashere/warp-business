a82f2030a6721ea39f1171700f0d99c2bef47517
Michael R. Schmidt <v-micschmidt@microsoft.com>
2026-04-11 21:37:52 -0700

    feat: Move all tables into 'warp' database schema (#10)
    Move all database tables into the 'warp' schema to support future multi-schema module architecture.

---
+++ WarpBusiness.Api/Data/Migrations/20260412043525_MoveToWarpSchema.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WarpBusiness.Api.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class MoveToWarpSchema : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "warp");
+
+            migrationBuilder.RenameTable(
+                name: "UserTenantMemberships",
+                newName: "UserTenantMemberships",
+                newSchema: "warp");
+
+            migrationBuilder.RenameTable(
+                name: "Users",
+                newName: "Users",
+                newSchema: "warp");
+
+            migrationBuilder.RenameTable(
+                name: "Tenants",
+                newName: "Tenants",
+                newSchema: "warp");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameTable(
+                name: "UserTenantMemberships",
+                schema: "warp",
+                newName: "UserTenantMemberships");
+
+            migrationBuilder.RenameTable(
+                name: "Users",
+                schema: "warp",
+                newName: "Users");
+
+            migrationBuilder.RenameTable(
+                name: "Tenants",
+                schema: "warp",
+                newName: "Tenants");
+        }
+    }
+}
